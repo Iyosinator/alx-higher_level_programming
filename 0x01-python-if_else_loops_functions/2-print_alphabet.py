5e90fd93d4af42e73fc8fff41606cf4f263a4422
Eyosias Mulugeta <iyosiasmulugeta@gmail.com>
2024-03-12 21:25:53 +0300

    Hot Fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
-print("".join(chr(letter) for letter in range(97, 123)))
+for i in range(ord('a'), ord('z') + 1):
+    print('{:c}'.format(i), end='')
